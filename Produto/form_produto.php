7f24d085397c57e77a4a60f74d6a3d7cade79ee7
LucasAbucaterFiorido <lucas.abucater@hotmail.com>
2022-08-29 22:21:57 -0300

    feat: new configs to introduce

--- Produto/form_produto.php
+++ Produto/form_produto.php
@@ -97,12 +97,30 @@
         </div>
         <br>
         <div class="row">
+            <div class="col-sm-3 border">
+                <img src="" alt="" id="preImg" style="max-width: 200px; max-height: 200px;">     
+            </div>
+            <div class="col-sm-9">
+                <label class="form-label" for="base64Code">Base 64:</label>
+                <br>
+                <textarea class="form-control" id="base64Code" name="base64Code" cols="140" rows="6"></textarea>
+            </div>
+        </div>
+        <br>
+        <div class="row">
             <div class="col-sm-12">
                 <label class="form-label" for="txtobs">Observação:</label>
                 <br>
                 <textarea class="form-control" id="txtobs" name="txtobs" cols="140" rows="4"></textarea>
             </div>
         </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-12">
+                <div id="callback"></div>
+            </div>
+        </div>
+        <br>
         <div class="row">
             <div class="col-sm-12 text-center">
                 <div class="card-footer">
@@ -114,3 +132,113 @@
             </div>
         </div>
     </form>
+    <script>
+        $(function(){
+        alert("teste");
+        var callback = $("#callback");
+        var action = "";
+
+        function loading(datas)
+        {
+            callback.empty().html("Carregando...");
+        }
+
+        function cheirinho_de_sucesso(datas)
+        {
+            callback.empty().html("<pre>"+ datas +"</pre>");
+            $("#txtcod").val($("#cod_pesquisa").html());
+        }
+
+        function erro_enviar(datas)
+        {
+            callback.empty().html("Erro ao enviar.");
+        }
+
+        // function limpar()
+        // {
+        //     $("#txtcod").val("");
+        //     $("#txtnome").val("");
+        //     $("#txtlogin").val("");
+        //     $("#txtsenha").val("");
+        //     $("#arquivoimg").val("");
+        //     $("#selectsts").val("");
+        //     $("#base64Code").val("");
+        //     $("#txtobs").val("");
+        // }
+
+        function successPesquisa(datas)
+        {
+            callback.empty().html("<pre>"+ datas +"</pre>");
+
+            $("#txtcod").val($("#cod_pesquisa").html());
+            $("#txtnome").val($("#nome_pesquisa").html());
+            $("#txtlogin").val($("#login_pesquisa").html());
+            $("#txtsenha").val($("#senha_pesquisa").html());
+            $('#base64Code').val($('#imagem_pesquisa').html());
+            // $("#arquivoimg").val($("#imagem_pesquisa").html());
+            $('#txtdata').val($('#cadastro_pesquisa').html());
+            $("#selectsts").val($("#status_pesquisa").html());
+            $("#txtobs").val($("#obs_pesquisa").html());
+
+            var preview = document.getElementById("preImg")
+
+            $("#base64Code").val($("#imagem_pesquisa").html());
+            preview.src = $("#imagem_pesquisa").html();
+        }
+
+        $.ajaxSetup({
+            type:           'POST',
+            beforeSend:     loading,
+            error:          erro_enviar,
+            success:        cheirinho_de_sucesso
+        })
+
+        $("#btt_pesquisar").click(function()
+        {
+            action= "Usuario/pesquisar_usuario.php";
+
+            $.ajax({
+                url:    action,
+                data:{
+                    txtcod: $("#txtcod").val()
+                },
+                success:    successPesquisa
+            });
+        });
+
+        $("#btt_cadastrar").click(function()
+        {
+            action = "Usuario/cadastrar_usuario.php";
+
+            $.ajax({
+                url:        action,
+                data:{
+                    txtnome: $("#txtnome").val(),
+                    txtlogin: $("#txtlogin").val(),
+                    txtsenha: $("#txtsenha").val(),
+                    arquivoimg: $("#base64Code").val(),
+                    selectsts: $("#selectsts").val(),
+                    txtobs: $("#txtobs").val()
+                }
+            });
+        });
+
+        $("#btt_alterar").click(function()
+        {
+            action = "Usuario/alterar_usuario.php";
+
+            $.ajax({
+                url:    action,
+                data:{
+                    txtcod: $("#txtcod").val(),
+                    txtnome: $("#txtnome").val(),
+                    txtlogin: $("#txtlogin").val(),
+                    txtsenha: $("#txtsenha").val(),
+                    arquivoimg: $("#base64Code").val(),
+                    selectsts: $("#selectsts").val(),
+                    txtobs: $("#txtobs").val()
+                }
+            });
+        });
+    });
+    </script>
